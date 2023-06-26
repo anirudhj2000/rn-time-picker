3e1530106a1507134a090415b3427f1991e4c2fe
anirudhj2000 <anirudhjoshi985@gmail.com>
2023-06-26 23:18:27 +0530

    initial commit

---
+++ src/timepicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View , Modal, Text, TouchableOpacity} from 'react-native'
+
+const TimePicker = () => {
+    return(
+        <View>
+            <Text>Time Picker</Text>
+        </View>
+    )
+}
+
+export default TimePicker
